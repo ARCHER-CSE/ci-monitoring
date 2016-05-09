7ea677866ced6155327bffab80578fe95aafb226
Jenkins CI <a.turner@epcc.ed.ac.uk>
2016-05-09 10:47:15 +0100

    Updated compiler tests to remove explicit directory specification

--- monitoring/compilers/gnu-compile.bash
+++ monitoring/compilers/gnu-compile.bash
@@ -1,11 +1,13 @@
 #/bin/bash --login
 
-cd /home/z01/z01/aturner/regtest/compilers
+pwd
 module swap PrgEnv-cray PrgEnv-gnu
 module list
 rm -f hello_mpi.x
 ftn -o hello_mpi.x hello_mpi.f90
 if [ -x "./hello_mpi.x" ]; then
+   ls -l hello_mpi.x
+   rm -f hello_mpi.x
    exit 0
 else
    exit 1
